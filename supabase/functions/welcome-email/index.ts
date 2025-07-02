6300f955b81f76a60f1791965b8a1e2c555feba7
Kabiru Haruna <kabiruharuna@Kabirus-MacBook-Pro.local>
2025-07-03 00:41:31 +0100

    update

--- supabase/functions/welcome-email/index.ts
+++ supabase/functions/welcome-email/index.ts
@@ -1,31 +1,31 @@
-const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
+// const RESEND_API_KEY = Deno.env.get('RESEND_API_KEY')
 
-Deno.serve(async (req: Request) => {
-  const { record } = await req.json()
+// Deno.serve(async (req: Request) => {
+//   const { record } = await req.json()
   
-  const email = record.email
-  const name = record.fullname
+//   const email = record.email
+//   const name = record.fullname
 
-  const res = await fetch('https://api.resend.com/emails', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${RESEND_API_KEY}`,
-    },
-    body: JSON.stringify({
-      from: 'The M3 Community <onboarding@resend.dev>',
-      to: email,
-      subject: 'Welcome to The M3 Community!',
-      html: `<strong>Hello ${name}, welcome to M3!</strong>`,
-    }),
-  })
+//   const res = await fetch('https://api.resend.com/emails', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Authorization: `Bearer ${RESEND_API_KEY}`,
+//     },
+//     body: JSON.stringify({
+//       from: 'The M3 Community <onboarding@resend.dev>',
+//       to: email,
+//       subject: 'Welcome to The M3 Community!',
+//       html: `<strong>Hello ${name}, welcome to M3!</strong>`,
+//     }),
+//   })
 
-  const data = await res.json()
+//   const data = await res.json()
 
-  return new Response(JSON.stringify(data), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-})
+//   return new Response(JSON.stringify(data), {
+//     status: 200,
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+// })
