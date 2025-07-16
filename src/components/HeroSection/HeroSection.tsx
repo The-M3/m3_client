5721f0deefcacbb02c99c3ff58049b3ab19bc426
Kabiru Haruna <kabiruharuna@Kabirus-MacBook-Pro.local>
2025-07-16 09:25:22 +0100

    update

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './HeroSection.module.scss';
+import Image from 'next/image';
 import { Button } from '../ui';
 import Link from 'next/link';
-import OptimizedImage from '../OptimizedImage/OptimizedImage';
 
 const HeroSection = () => {
   return (
@@ -13,7 +13,7 @@ const HeroSection = () => {
       <div className={styles.descriptionContainer}>
         <div className={styles.imageContainer}>
           <div className={styles.heroImage}>
-            <OptimizedImage
+            <Image
               src="/images/heroImage.jpeg"
               alt="Four professionally dressed African professionals—two men and two women—smiling and gathered around a laptop, engaged in a collaborative discussion."
               fill
