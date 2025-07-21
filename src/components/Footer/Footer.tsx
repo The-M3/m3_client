5008ae724f3cbf0f71303876d57c6a831cfbd463
Kabiru Haruna <kabiruharuna@Kabirus-MacBook-Pro.local>
2025-07-21 16:13:57 +0100

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,32 +6,42 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        {/* Left Section - Contact & Links */}
-        <div className={styles.leftSection}>
-          <a href="mailto:josh@TheM3.com" className={styles.email}>
-            josh@TheM3.com
+        {/* <div className={styles.leftSection}>
+          <a href="mailto:hello@them3.co" className={styles.email}>
+          hello@them3.co 
           </a>
           <nav className={styles.nav}>
             <a href="#" className={styles.navLink}>Regional hubs information</a>
             <a href="#" className={styles.navLink}>Privacy policy</a>
             <a href="#" className={styles.navLink}>Terms of service</a>
           </nav>
-        </div>
+        </div> */}
 
-        {/* Center Section - Logo */}
         <div className={styles.centerSection}>
-          <div className={styles.logo}>
             <Image src="/svgs/m3_footer_logo.svg" alt="The M3" width={130} color='#DAA765' height={130} priority />
-          </div>
         </div>
 
-        {/* Right Section - Social Media */}
         <div className={styles.rightSection}>
           <nav className={styles.socialNav}>
-            <a href="#" className={styles.socialLink}>Facebook</a>
-            <a href="#" className={styles.socialLink}>Instagram</a>
-            <a href="#" className={styles.socialLink}>Twitter</a>
-            <a href="#" className={styles.socialLink}>LinkedIn</a>
+          <a href="mailto:hello@them3.co" className={styles.email}>
+          hello@them3.co 
+          </a>
+            <a target="_blank" href="https://www.youtube.com/@TheM3_hq" className={styles.socialLink}>
+            <Image src="/svgs/youtube-icon.svg" alt="The M3" width={20} height={20} priority />
+            
+            </a>
+            <a target="_blank" href="https://www.instagram.com/them3_hq/" className={styles.socialLink}>
+            <Image src="/svgs/instagram-icon.svg" alt="The M3" width={20} height={20} priority />
+            
+            </a>
+            <a target="_blank" href="https://x.com/TheM3_HQ" className={styles.socialLink}>
+            <Image src="/svgs/x-icon.svg" alt="The M3" width={20} height={20} priority />
+
+            </a>
+            <a target="_blank" href="https://www.linkedin.com/company/the-m3/" className={styles.socialLink}>
+            <Image src="/svgs/linkedin-icon.svg" alt="The M3" width={20} height={20} priority />
+
+            </a>
           </nav>
         </div>
       </div>
